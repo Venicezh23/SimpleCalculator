b7fa5387bdbfd79c7d5483f9e60b95939653af69
Venicezh23 <83906230+Venicezh23@users.noreply.github.com>
2024-06-13 16:01:33 +0800

    Add project files.

---
+++ SimpleCalculator/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleCalculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to my simple calculator!");
+
+            Console.Write("Enter the first number: ");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter the second number: ");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+
+            double result = num1 + num2;
+
+            Console.WriteLine("Sum of num1 and num2: " + result);
+
+            Console.ReadLine();
+        }
+    }
+}
